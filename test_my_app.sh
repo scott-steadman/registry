38a35e821f8b25ef0b4ce1befeab61f44f1860e8
Scott Steadman <scott.steadman@geni.com>
2025-11-19 14:51:04 -0800

    cleanup test_my_app.sh

--- test_my_app.sh
+++ test_my_app.sh
@@ -3,6 +3,11 @@
 unset RBENV_VERSION
 export RBENV_ROOT=/opt/rbenv
 export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
+export SKIP_COVERAGE="true"
+
+# Show which tests are being run and their results
+export TEST_OPTS="--verbose --no-show-detail-immediately"
+#export TEST_OPTS="--verbose --no-show-detail-immediately --stop-on-failure"
 
 git gc
 
@@ -14,11 +19,6 @@ bundle config --local without vscode
 rm Gemfile.lock
 bundle install
 
-# Show which tests are being run and their results
-export TEST_OPTS="--verbose --no-show-detail-immediately"
-#export TEST_OPTS="--verbose --no-show-detail-immediately --stop-on-failure"
-export SKIP_COVERAGE="true"
-
-rm -f db/*.sqlite3
+rm -f db/*.sqlite3 public/coverage
 bundle exec rake db:create db:migrate
 bundle exec rake db:test:prepare test
