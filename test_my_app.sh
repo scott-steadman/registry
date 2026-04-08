330b4b0a4e0af40e347a3fc01f148ce200446191
Scott Steadman <scott.steadman@geni.com>
2026-04-08 15:51:41 -0700

    cleanup test_my_app.sh
      Jenkins will call this.

--- test_my_app.sh
+++ test_my_app.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-unset RBENV_VERSION
-export RBENV_ROOT=/opt/rbenv
-export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
-export SKIP_COVERAGE="true"
-
 # Show which tests are being run and their results
 export TEST_OPTS="--verbose --no-show-detail-immediately"
 #export TEST_OPTS="--verbose --no-show-detail-immediately --stop-on-failure"
-
-git gc
 
 bundle config --local build.sqlite3 "--enable-system-libraries"
 bundle config --local clean true
