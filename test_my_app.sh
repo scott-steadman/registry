1ce98dc4d1e976a968af87f0dbad8c94215a54fb
Scott Steadman <scott.steadman@geni.com>
2026-04-22 12:13:06 -0700

    cleanup test_my_app.sh
      I also added a --no-clean switch to help claude test faster.

--- test_my_app.sh
+++ test_my_app.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-unset RBENV_VERSION
-export RBENV_ROOT=/opt/rbenv
-export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
 export SKIP_COVERAGE="true"
 
 # Show which tests are being run and their results
 export TEST_OPTS="--verbose --no-show-detail-immediately"
 #export TEST_OPTS="--verbose --no-show-detail-immediately --stop-on-failure"
 
-git gc
-
 bundle config --local build.sqlite3 "--enable-system-libraries"
 bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
-rm Gemfile.lock
-bundle install
+# Only clean and reinstall if --no-clean is not specified
+if [[ "$*" != *--no-clean* ]]; then
+  git gc
 
-rm -f db/*.sqlite3 public/coverage
-bundle exec rake db:create db:migrate
-bundle exec rake db:test:prepare test
+  rm -rf Gemfile.lock vendor/bundle
+  bundle _1.17.3_ install
+fi
+
+rm -rf db/*.sqlite3 public/coverage
+RAILS_ENV=test bundle _1.17.3_ exec rake db:create db:migrate
+RAILS_ENV=test bundle _1.17.3_ exec rake test
