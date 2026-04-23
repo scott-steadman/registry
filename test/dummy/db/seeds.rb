a343fa5d293ea51e8037cc7b5c3fc24ebfdb7676
Scott Steadman <scott.steadman@geni.com>
2026-04-23 12:12:21 -0700

    cleanup db/seeds.rb

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -10,7 +10,8 @@ template = {
     :number   => 1,
     :string   => 'string',
   }
-}
+}.stringify_keys
 
 puts "Seeding dummy database with template: #{template.inspect}"
+Registry::Entry.delete_all
 Registry::Entry.root(Rails.env).merge(template)
