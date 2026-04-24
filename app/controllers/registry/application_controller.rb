137f3f896ba091739d4f752a1a570fff31e9110f
Scott Steadman <scott.steadman@geni.com>
2026-04-24 11:08:02 -0700

    fix dummy app for rails-3.2

--- app/controllers/registry/application_controller.rb
+++ app/controllers/registry/application_controller.rb
@@ -1,5 +1,10 @@
 module Registry
   class ApplicationController < ActionController::Base
     include Engine.routes.url_helpers
+
+# Useful for debugging errors that don't show up in the logs/console
+#    rescue_from Exception do |e|
+#      pp :error, e, e.backtrace
+#    end
   end
 end
